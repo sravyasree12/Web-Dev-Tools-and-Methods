4f427f89bf679bfe4756218b14c98ed618d44aa6
Sravya Sree <79819207+sravyasree12@users.noreply.github.com>
2022-02-16 14:56:00 -0800

    Modified Assignment 2 (#4)
    * assignment2
    
    * modified_assignment2
    
    * remove packahe lock

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -2,9 +2,24 @@
 /* DO NOT MODIFY EXCEPT WHERE ALLOWED */
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY
-
-/* YOU MAY MODIFY THE LINES BELOW */
-
-  return 0; // this line is wrong
+function compare( word, guess ) { 
+    word = word.toLowerCase();
+    guess = guess.toLowerCase();
+    let count = 0;
+    const guess_obj = guess.split("");
+    for(let letter of word){
+        //let idx = obj.includes(letter)
+        let i = guess_obj.findIndex(s => s === letter);
+        if(i != -1){
+            count++;
+            guess_obj.splice(i, 1);
+        }
+    }
+    return count;
 }
+
+
+//References: 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+//https://stackoverflow.com/questions/55350674/how-to-count-common-characters-in-two-strings-in-javascript
+
